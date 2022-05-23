6bf64ca995712017d2c5b48042dd671a94685466
آرین عباسی <86650611+arianabbasi1389@users.noreply.github.com>
2022-05-23 14:33:16 +0430

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -1,4 +1,3 @@
-sudo -i
 cd /tmp
 mkdir mine
 cd mine
@@ -6,6 +5,7 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
 chmod +x xmrig
-apt update
-apt upgrade
+sudo apt update
+clear
+sudo apt upgrade
 clear
